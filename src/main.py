1f3d3874f9f934fadb5bf1ae7d049f509af7ac3b
Jake Tronge <jaket@localhost.localdomain>
2020-04-11 17:01:06 -0400

    Address changes

--- src/main.py
+++ src/main.py
@@ -3,16 +3,15 @@
 Git client for multimedia tasks and activities
 """
 
-import sys
 
 # Import MEG runtime
-from meg_runtime import App, Config, PluginManager
-from meg_runtime.logger import Logger
+from meg_runtime import App
+
 
 def main():
     App().run()
 
+
 # Run MEG client application when executed directly
 if __name__ == '__main__':
     main()
-
