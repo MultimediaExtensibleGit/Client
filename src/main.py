24386257d45b3e16c1d717955e7b044e7b7c7d1a
Kyle Pugh <kyletpugh@users.noreply.github.com>
2020-04-13 01:20:39 -0400

    Change application to singleton

--- src/main.py
+++ src/main.py
@@ -3,15 +3,10 @@
 Git client for multimedia tasks and activities
 """
 
-
 # Import MEG runtime
 from meg_runtime import App
 
 
-def main():
-    App().run()
-
-
 # Run MEG client application when executed directly
 if __name__ == '__main__':
-    main()
+    App.run()
